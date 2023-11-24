a344ef6b982158667ff6f00c7b1cf13b8bf388db
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-23 21:50:10 -0300

    feat: creating a function to validate the file

--- src/file.js
+++ src/file.js
@@ -1,6 +1,28 @@
+const { error } = require('./constant')
+const { readFile } = require('fs/promises')
+
+const DEFAULT_OPTION = {
+    maxLine:3,
+    fields: ['id', 'name', 'profession', 'age']
+}
+
 class File {
     static async csvToJson(filePath) {
-        
+        const content = await readFile(filePath, 'utf8')
+        const validation = this.isValid(content)
+
+        if (!validation.valid) throw Error(validation.error)
+    }
+
+    static isValid(csvString, option = DEFAULT_OPTION) {
+        const [headers, ...linesWithoutHeader] = csvString.split(/\r?\n/)
+
+        if (linesWithoutHeader.length === 0) {
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false
+            }
+        }
     }
 }
 
