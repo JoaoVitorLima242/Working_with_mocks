2d1f9bb06be08c5320604b27b0ceae8241b0a15e
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-23 22:02:15 -0300

    feat: validating content

--- index.test.js
+++ index.test.js
@@ -11,5 +11,33 @@ const assert = require('assert')
 
             await assert.rejects(result, expected)
         }
+
+        {
+            const filePath = './mocks/invalid-header.csv'
+            const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE)
+            const result = File.csvToJson(filePath)
+
+            await assert.rejects(result, expected)
+        }
+        
+        {
+            const filePath = './mocks/fiveItems-invalid.csv'
+            const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+            const result = File.csvToJson(filePath)
+
+            await assert.rejects(result, expected)
+        }
+        {
+            const filePath = './mocks/threeItems-valid.csv'
+            const expected = [
+                {"id": 1, "name": "João", "profession": "Engenheiro", "age": 30},
+                {"id": 2, "name": "Maria", "profession": "Médica", "age": 35},
+                {"id": 3, "name": "Carlos", "profession": "Professor", "age": 40}
+            ]
+            
+            const result = File.csvToJson(filePath)
+
+            assert.deepEqual(result, expected)
+        }
     }
 )()
--- src/file.js
+++ src/file.js
@@ -12,17 +12,39 @@ class File {
         const validation = this.isValid(content)
 
         if (!validation.valid) throw Error(validation.error)
+
+        return this.parseCSVToJSON(content)
+    }
+
+    static parseCSVToJSON(csvString) {
+        return csvString
     }
 
     static isValid(csvString, option = DEFAULT_OPTION) {
         const [headers, ...linesWithoutHeader] = csvString.split(/\r?\n/)
 
-        if (linesWithoutHeader.length === 0) {
+        const isEmptyFile = linesWithoutHeader.length === 0
+        const isHeaderInvalid = headers !== option.fields.join()
+        const isFileLinesExceedingMaxLine = linesWithoutHeader.length > option.maxLine
+
+        if (isEmptyFile || isFileLinesExceedingMaxLine) {
             return {
                 error: error.FILE_LENGTH_ERROR_MESSAGE,
                 valid: false
             }
         }
+
+        if (isHeaderInvalid) {
+            return {
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        return {
+            valid: true
+        }
+
     }
 }
 
